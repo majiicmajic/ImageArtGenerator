3b687cd4173d4642d4f63131d0e276d2e427997b
majic <65995144+majiicmajic@users.noreply.github.com>
2022-03-29 10:07:18 +0100

    Added new navigation

---
+++ _nuxt/static/1648544759/Manage/resendemaillink/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648544759",layout:"loginLayout",error:a,state:{AuthdisplayText:"initials"},serverRendered:true,routePath:"\u002Fmanage\u002Fresendemaillink",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1648544759/majicAI/method2/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/majicAI/method2", {data:[{}],fetch:{},mutations:[]});
